4a022226a207dec7fe1522fdea619b936e6ecdae
youcisla <chehbouby@yahoo.fr>
2022-11-26 21:38:03 +0100

    css

--- view/header.php
+++ view/header.php
@@ -16,7 +16,100 @@
                 <td>
                     <h1 class="logo">
                         <a class="button1" href="index.php?page=home">
-                            <img src="logo.png" type="image/png"></img>
+                            <!-- <img src="logo.png" type="image/png"></img> -->
+                            <style>
+                            .prof {
+                                font-family: "Lucida Console", "Courier New", Monospace;
+                                width: 50px;
+                                height: 50px;
+                                box-shadow: 16px 14px 20px #0000008c;
+                                border-radius: 180px;
+                                position: relative;
+                                overflow: hidden;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                right: 75%;
+                                color: #ff0052;
+                                text-decoration: none;
+                                text-transform: uppercase;
+                            }
+
+                            .prof::before {
+                                background-image: conic-gradient(#ff0052 20deg,
+                                        transparent 120deg);
+                                width: 150%;
+                                height: 150%;
+                                position: absolute;
+                                animation: rotate 2s linear infinite;
+                            }
+
+                            .prof::after {
+                                width: 40px;
+                                height: 40px;
+                                background: #101010;
+                                position: absolute;
+                                border-radius: 10px;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                color: #ff0052;
+                                font-size: larger;
+                                letter-spacing: 5px;
+                                box-shadow: inset 20px 20px 20px #0000008c;
+                            }
+
+                            div {
+                                width: 200px;
+                                height: 80px;
+                                box-shadow: 16px 14px 20px #0000008c;
+                                border-radius: 10px;
+                                position: relative;
+                                overflow: hidden;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                font-size: 16px;
+                            }
+
+                            div::before {
+                                content: "";
+                                background-image: conic-gradient(#ff0052 20deg,
+                                        transparent 120deg);
+                                width: 250%;
+                                height: 250%;
+                                position: absolute;
+                                /* -webkit-animation: rotate 2s linear infinite; */
+                                animation: rotate 2s linear infinite;
+                            }
+
+                            div::after {
+                                content: 'Le Bon Choix';
+                                width: 190px;
+                                height: 70px;
+                                background: #101010;
+                                position: absolute;
+                                border-radius: 10px;
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                color: #ff0052;
+                                font-size: larger;
+                                letter-spacing: 5px;
+                                box-shadow: inset 20px 20px 20px #0000008c;
+                            }
+
+                            @keyframes rotate {
+                                0% {
+                                    transform: rotate(0deg);
+                                }
+
+                                100% {
+                                    transform: rotate(-360deg);
+                                }
+                            }
+                            </style>
+                            <div></div>
                         </a>
                     </h1>
                 </td>
