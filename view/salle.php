68c4014e17ca77314fbc5e68a6fd001e375c7727
salah127 <salah1999jari@gmail.com>
2022-12-01 15:26:17 +0100

    push

--- view/salle.php
+++ view/salle.php
@@ -33,32 +33,46 @@
                 afficher_salle($_GET['numero']);
 
 
+                if (isset($_SESSION["connecte"]) && $_SESSION["connecte"]){
 
-                if ($_SESSION["role"]=='pro'){
-                    echo '
+                    if ($_SESSION["role"]=='Pro'){
+                        echo '
+                        <br>
+                        <br>
+                        <div class="salasghira2 btnajoutsalle2">
+                            <a class="cnx-sub" href="index.php?page=">Modifier</a>
+                        </div>
+                        <br>
+                        <br>
+                        <div class="salasghira2 btnajoutsalle2">
+                            <a class="cnx-sub" href="index.php?page=">Supprimer</a>
+                        </div>
+                        ';
+                }   
+                    else{
+                    echo '  
                     <br>
                     <br>
                     <div class="salasghira2 btnajoutsalle2">
-                        <a class="cnx-sub" href="index.php?page=">Modifier</a>
+                        <a class="cnx-sub" href="index.php?page=calendar">Reserver</a>
                     </div>
                     <br>
-                    <br>
-                    <div class="salasghira2 btnajoutsalle2">
-                        <a class="cnx-sub" href="index.php?page=">Supprimer</a>
-                    </div>
-';
-                    
-                }
+                    ';
+                }}
                 else{
-                    echo '<br>
-                    <br>
-                    <div class="salasghira2 btnajoutsalle2">
-                        <a class="cnx-sub" href="index.php?page=">Reserver</a>
-                    </div>
-                    <br>
+                        // alert("Connectez vous avant de reserver une salle.");
+                    echo '  <br>
+                            <br>
+                                <div class="salasghira2 btnajoutsalle2">
+                                    <a class="cnx-sub" href="index.php?page=connexion">Reserver</a>
+                                </div>
+                                <br>
                             ';
+                    }
+                
                     
-                }
+                
             ?>
+        </table>
     </form>
 </fieldset>
