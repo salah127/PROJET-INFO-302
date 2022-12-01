137316cbf049830f70e678b572c27768269d9800
salah127 <salah1999jari@gmail.com>
2022-12-01 23:54:22 +0100

    css

--- view/profil.php
+++ view/profil.php
@@ -1,7 +1,8 @@
-<form action = "." method = "post" class="menu">                                <ul class="CreRank">
-                                    <li ><a class="button" href ="index.php?page=supprimer">Supprimer</a></li>
-                                    <li ><a class="button" <?php if ($_SESSION["role"]=='internaute'){ echo'href="index.php?page=modifier"' ;} else{echo'href ="index.php?page=pro-modif"';}  ?>>Modifer</a></li>
-                                </ul>
+<form action="." method="post">
+    <div class="supprof">
+        <li><a class="supprof1" href="index.php?page=supprimer">Supprimer</a></li>
+        <li><a class="supprof2"
+                <?php if ($_SESSION["role"]=='internaute'){ echo'href="index.php?page=modifier"' ;} else{echo'href ="index.php?page=pro-modif"';}  ?>>Modifer</a>
+        </li>
+    </div>
 </form>
-
-
