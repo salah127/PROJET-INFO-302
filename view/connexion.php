a8e14173a5ecdd6f87c971db4c3981c6e298d8e0
salah127 <salah1999jari@gmail.com>
2022-12-06 04:38:53 +0100

    css

--- view/connexion.php
+++ view/connexion.php
@@ -1,15 +1,29 @@
 <fieldset>
-    <legend><h2>Connectez vous</h2></legend>
-<form action = "index.php" method = "post">
-    <input type="hidden" name="action" value="connexion">
-    <label for="idpseudocnx">Pseudo :</label>
-    <input type = "text" id="idpseudocnx" class="cnx-control" name="pseudo" placeholder="entrer votre pseudo...">
-    <br>
-    <label for="idpasscnx">Mot de passe :</label>
-    <input type = "password" class="cnx-control" name="mdp" id="idpasscnx" placeholder="entrer votre mot de passe...">
-<br>
-    <input type = "submit" class="cnx-sub" value = "connexion">
-</form>
+    <legend class='titleliste'>
+        <h2>Connectez vous</h2>
+    </legend>
+    <form action="index.php" method="post">
+        <input type="hidden" name="action" value="connexion">
+        <div id="second">
+            <div class="mom">
+                <div id="first">
+                    <label for="idpseudocnx">Pseudo :</label>
+                </div>
+                <input type="text" id="idpseudocnx" class="cnx-control" name="pseudo"
+                    placeholder="entrer votre pseudo...">
+            </div>
+            <br>
+            <div class="mom">
+                <div id="first">
+                    <label for="idpasscnx">Mot de passe :</label>
+                </div>
+                <input type="password" class="cnx-control" name="mdp" id="idpasscnx"
+                    placeholder="entrer votre mot de passe...">
+            </div>
+        </div>
+        <br>
+        <input type="submit" class="cnx-sub" value="connexion">
+    </form>
 </fieldset>
 <a href="?page=inscription" role="button">creer un compte internaute</a>
 <br>
