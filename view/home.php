4dc5d3ee863a84307ac2283ce16d5541bca912e3
salah127 <salah1999jari@gmail.com>
2022-12-15 05:17:28 +0100

    here

--- view/home.php
+++ view/home.php
@@ -11,7 +11,6 @@
                                 <img src="image/' . $row["photo"] . '" width = "50px" height = "50px" />
                             </div>
                             <div class="card-body">
-<<<<<<< HEAD
                                 <h4>' . $row["numero"] . ' </h4> 
                                 <p> '. $row["Description"] . ' </p>
                                 <div class="user">
@@ -20,14 +19,6 @@
                                         <small>'.$row["time_ajout"].'</small>
                                     </div>
                                 </div>  
-=======
-                                <div class="hna">
-                                    <h4>Salle N°: ' . $row["numero"] . '</h4> 
-                                </div>
-                                <div class="hna">
-                                    <p class="pcard"> <a href="index.php?page=salle&id_salle=' . $row["id_salle"] . '">Plus</a> </p> 
-                                </div>
->>>>>>> 40d0bdf516e6c6a34671e2e98cf68b881d99aa32
                             </div>
                             </a>
                         </div>
--- view/salle.php
+++ view/salle.php
@@ -1,41 +1,9 @@
 <?php
     $_SESSION['id_salle'] = $_GET['id_salle'];
 ?>
-<<<<<<< HEAD
-
-<fieldset class='batima2'>
-    <legend class='titleliste'>
-        <h2>   Salle <?php echo '<nav>'. $_GET['id_salle'] . '</nav> '; ?></h2>
-    </legend>
-    
-        <table class=tab>
-            <tr>
-                <td class=tab>
-                    <li>
-                        <div class='card3'><strong>Numero</strong></div>
-                    </li>
-                </td>
-                <td class=tab>
-                    <li>
-                        <div class='card3'><strong>photo</strong></div>
-                    </li>
-                </td>
-                <td class=tab>
-                    <li>
-                        <div class='card3'><strong>Capacite</strong></div>
-                    </li>
-                </td>
-                <td class=tab>
-                    <li>
-                        <div class='card3'><strong>Niveau requis</strong></div>
-                    </li>
-                </td>
-            </tr>
-=======
 <div class="containerSalle">
     <form action="." method="post">
         <div class="cardSalle">
->>>>>>> 40d0bdf516e6c6a34671e2e98cf68b881d99aa32
             <?php
             $tab = salle($_GET['id_salle']);
             if ($tab == NULL){
@@ -83,53 +51,16 @@
                     }   
                     else{
                             echo '  
-<<<<<<< HEAD
-                            <br>
-                            <br>
-                            <div class="salasghira2 btnajoutsalle2">
-                                <a class="cnx-sub"  href="?page=calendrier&id_salle='.$_SESSION['id_salle'].'">Resever</a>
-                            </div>
-                            <br>
-=======
                             <div class="btnSalleDiv">
                                 <input type = "submit" class="btnSalle" value = "resever">
                             </div>   
->>>>>>> 40d0bdf516e6c6a34671e2e98cf68b881d99aa32
                             ';
                     }
                 }else{
                     echo '  
-<<<<<<< HEAD
-                    <br>
-                    <br>
-                    <div class="salasghira2 btnajoutsalle2">
-                        <a class="cnx-sub"  href="?page=calendrier&id_salle='.$_SESSION['id_salle'].'">Resever</a>
-                        <br><br><br><br>';
-                        if(exist_org($_SESSION["id"][0],$_SESSION["id_salle"])==0){
-                            echo'
-                            <form action="." method="post">
-                        <input type="hidden" name="action" value="organisateur">
-                        <input type="submit" class="cnx-sub" value="Devenir Organisateur">
-                        </form>';
-                        
-                        }else{
-                            echo'
-                            <form action="." method="post">
-                            <input type="hidden" name="action" value="Dimissioné">
-                            <input type="submit" class="cnx-sub" value="Dimissioné">
-                            </form>';
-
-                        }
-
-                        echo'
-                        <br><br><br><br>
-                        <a class="cnx-sub"  href="index.php?page=ajout-ressource">Ajouter des ressources</a>
-                        
-=======
                     <div class="btnSalleDiv">
                         <a class="btnSalle" href="?page=calendrier&id_salle='.$_SESSION['id_salle'].'">resever</a>
                         <a class="btnSalle" href="index.php?page=calendrier">ajouter des ressources</a>
->>>>>>> 40d0bdf516e6c6a34671e2e98cf68b881d99aa32
                     </div>
                     ';
                     }
@@ -142,12 +73,7 @@
                             ';
                 }
             ?>
-<<<<<<< HEAD
-    
-</fieldset>
-=======
         </div>
     </form>
     </fieldset>
 </div>
->>>>>>> 40d0bdf516e6c6a34671e2e98cf68b881d99aa32
